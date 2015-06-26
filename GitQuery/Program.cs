b6b2783ba36f56251f2ea14121175c7d550bb2b3
Radu Iscu <linkerro@gmail.com>
2015-06-26 13:59:42 +0300

    First commit

---
+++ GitQuery/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+
+namespace GitQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var repo = new Repository(@"C:\Users\linke\Documents\GitHub\enki.js"))
+            {
+                var RFC2822Format = "ddd dd MMM HH:mm:ss yyyy K";
+
+                foreach (Commit c in repo.Commits)
+                {
+                    Console.WriteLine($"commit {c.Id}");
+
+                    if (c.Parents.Count() > 1)
+                    {
+                        Console.WriteLine("Merge: {0}",
+                            string.Join(" ", c.Parents.Select(p => p.Id.Sha.Substring(0, 7)).ToArray()));
+                    }
+
+                    Console.WriteLine($"Author: {c.Author.Name} <{c.Author.Email}>");
+                    Console.WriteLine("Date:   {0}", c.Author.When.ToString(RFC2822Format, CultureInfo.InvariantCulture));
+                    Console.WriteLine();
+                    Console.WriteLine(c.Message);
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
